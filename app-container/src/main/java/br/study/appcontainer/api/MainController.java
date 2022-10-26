18250b695f20c8d3156ce9a9aada63381c3e6d43
Gian Milani <gianmilani@gmail.com>
2022-10-25 22:51:06 -0300

    feat(application): removed imports unused

--- app-container/src/main/java/br/study/appcontainer/api/MainController.java
+++ app-container/src/main/java/br/study/appcontainer/api/MainController.java
@@ -2,14 +2,10 @@ package br.study.appcontainer.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 public class MainController {
-
 
     @GetMapping
     public String index() {
