b44be6cdc158975106162f21e673ad826b5f532e
Gian Milani <gianmilani@gmail.com>
2022-10-25 22:49:41 -0300

    feat(application): add new route

--- app-container/src/main/java/br/study/appcontainer/api/MainController.java
+++ app-container/src/main/java/br/study/appcontainer/api/MainController.java
@@ -1,7 +1,11 @@
 package br.study.appcontainer.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class MainController {
@@ -15,5 +19,10 @@ public class MainController {
     @GetMapping("/health")
     public String healthCheck() {
         return "OK";
+    }
+
+    @GetMapping("/message/{name}")
+    public String getName(@PathVariable String name){
+        return "Welcome %s !!!!".formatted(name);
     }
 }
