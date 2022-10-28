59b72f2b5ee08ed7f89e6f3a8c5fc04ac100b371
Gian Milani <gianmilani@gmail.com>
2022-10-28 08:20:39 -0300

    feat(app-spring) : added new route, and properties file

--- app-container/src/main/java/br/study/appcontainer/api/MainController.java
+++ app-container/src/main/java/br/study/appcontainer/api/MainController.java
@@ -1,11 +1,18 @@
 package br.study.appcontainer.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${user.age}")
+    private String userAge;
 
     @GetMapping
     public String index() {
@@ -20,5 +27,10 @@ public class MainController {
     @GetMapping("/message/{name}")
     public String getName(@PathVariable String name){
         return "Welcome %s !!!!".formatted(name);
+    }
+
+    @GetMapping("/env")
+    public  String getEnv(){
+        return "User : " + userName + " | Age : " + userAge;
     }
 }
