fc90d18ea8bb727bd1beacc18b58a9759e0b16c2
Gian Milani <gianmilani@gmail.com>
2024-06-16 21:24:30 -0300

    feat: Config env basic - added new rote in app - added env property in deployment

--- application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
+++ application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
@@ -1,13 +1,22 @@
 package br.study.demo_kube.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/home")
-class Home {
+class Home(
+    @Value("\${app.name}")
+    private val appName: String,
+    @Value("\${app.info}")
+    private val appInfo: String
+) {
 
     @GetMapping
     fun welcome() = "Hello, welcome v2"
+
+    @GetMapping("/show")
+    fun showVariables() = "AppName= $appName | AppInfo= $appInfo"
 }
