fe6320e22b3941b3ea69351d09d2481a5abf06f7
Gian Milani <gianmilani@gmail.com>
2024-06-24 22:19:30 -0300

    s test

--- application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
+++ application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
@@ -1,5 +1,6 @@
 package br.study.demo_kube.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,12 +12,18 @@ class Home(
     @Value("\${app.name}")
     private val appName: String,
     @Value("\${app.info}")
-    private val appInfo: String
+    private val appInfo: String,
+    @Value("\${app.test_sc}")
+    private val testSc: String
 ) {
+    private var log = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping
     fun welcome() = "Hello, welcome v2"
 
     @GetMapping("/show")
     fun showVariables() = "AppName= $appName | AppInfo= $appInfo"
+
+    @GetMapping("/exec")
+    fun showInfoSec() = log.info("Show info sec {}", testSc)
 }
