2a6d48a9b94efc69ffc77bab6f1addbd632ac270
Gian Milani <gianmilani@gmail.com>
2024-06-24 23:38:05 -0300

    fixed liveness probe

--- application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
+++ application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+<<<<<<< HEAD
 import java.util.Random
+=======
+import java.time.LocalTime
+>>>>>>> 586944c (added livenessProbe)
 
 @RestController
 @RequestMapping("/home")
@@ -18,6 +22,7 @@ class Home(
     private val testSc: String
 ) {
     private var log = LoggerFactory.getLogger(this::class.java)
+    private val timeStart = LocalTime.now().plusSeconds(10)
 
     @GetMapping
     fun welcome() = "Hello, welcome v2"
@@ -30,8 +35,12 @@ class Home(
 
     @GetMapping("/healthCheck")
     fun health(): String {
+<<<<<<< HEAD
         val nextInt = Random().nextInt(1, 100)
         if(nextInt > 50) throw RuntimeException("Application with problem")
+=======
+        if(LocalTime.now() < timeStart) throw RuntimeException("application error")
+>>>>>>> 586944c (added livenessProbe)
         return "OK"
     }
 }
