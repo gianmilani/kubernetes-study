356a57f183cbc8e8bcfe901dac6602344431da4e
Gian Milani <gianmilani@gmail.com>
2024-06-24 23:00:30 -0300

    added livenessProbe

--- application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
+++ application/demo-kube/src/main/kotlin/br/study/demo_kube/controller/Home.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Random
 
 @RestController
 @RequestMapping("/home")
@@ -26,4 +27,11 @@ class Home(
 
     @GetMapping("/exec")
     fun showInfoSec() = log.info("Show info sec {}", testSc)
+
+    @GetMapping("/healthCheck")
+    fun health(): String {
+        val nextInt = Random().nextInt(1, 100)
+        if(nextInt > 50) throw RuntimeException("Application with problem")
+        return "OK"
+    }
 }
